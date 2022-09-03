8a249a458a47e7aa7b6f45f05c578db9536bd2bc
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-09-03 11:11:14 +0300

    created expenses chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import './Expenses.css';
 import Card from "../UI/Card";
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 function Expenses(props) {
 
@@ -19,6 +20,7 @@ function Expenses(props) {
   return (
     <Card className="expenses">
     <ExpensesFilter onApplyFilterOption = {applyFilterOption} initialOption = {enteredFilterOption}/>
+    <ExpensesChart expenses={filteredExpensesArray}/>
     <ExpensesList listData = {filteredExpensesArray}/>
     </Card>
   );
